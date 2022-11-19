3f136ac6e9402530df0f00b7a66c54ffee0793e9
王卫 <atwwei@gmail.com>
2022-11-19 10:24:32 +0800

    use GPT_LOADER

--- projects/dfp/src/app/app.module.ts
+++ projects/dfp/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { GPT_LOADER, GPT_SOURCE } from '@wwei/dfp';
+import { EMPTY, interval, of } from 'rxjs';
+import { delay, switchMap, take } from 'rxjs/operators';
+
 import { environment } from '../environments/environment';
 import { BaseComponent } from './base.component';
 import { AppComponent } from './app.component';
@@ -19,7 +23,18 @@ import { AppRoutingModule } from './app-routing.module';
       registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: GPT_LOADER,
+      useValue: interval(200).pipe(
+        switchMap(() =>
+          document.readyState == 'complete' ? of(GPT_SOURCE.STANDARD) : EMPTY,
+        ),
+        delay(500),
+        take(1),
+      ),
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
